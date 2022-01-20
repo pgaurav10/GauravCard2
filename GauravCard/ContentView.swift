268672c1eee4b14df34abc63d7c63545ac3a4a07
Gaurav <gauravpatil24121997@gmail.com>
2022-01-19 19:10:10 -0600

    Inital Draft

--- GauravCard/ContentView.swift
+++ GauravCard/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.17, green: 0.24, blue: 0.31, opacity: 1.00)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("GauravPatil")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 190)
+                    .clipShape(Circle())
+                Text("Gaurav Patil ")
+                    .font(Font.custom("Pacifico-Regular", size: 30))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Software Engineer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+            }
+        }
+        
     }
 }
 
